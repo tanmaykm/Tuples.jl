b4c2d65c161080fca33ee06e7d8f2709609a4d30
Matt Bauman <mbauman@gmail.com>
2015-06-16 17:35:00 -0400

    Add methods for computation in the type domain
    This allows Julia to infer the returned type for short Tuples

--- src/TupleTypes.jl
+++ src/TupleTypes.jl
@@ -4,6 +4,40 @@ import Base: isvatuple
 
 check(T) = (T===Tuple || T===NTuple) && throw(ArgumentError("parameters of $T are undefined"))
 
+# Define methods for up to N tuple parameters to be evaluated in the Type domain
+const N=4
+ie = :()
+for p=0:N
+    params = ntuple(i->symbol(:P,i), p)
+    global getpara, concatenate
+    # Return the whole parameter list as an svec
+    @eval getpara{$(params...)}(::Type{Tuple{$(params...)}}) = Base.svec($(params...))
+
+    # Accessing by a constant value
+    for i=1:p
+        @eval getpara{$(params...)}(::Type{Tuple{$(params...)}}, ::Type{Val{$i}}) = $(params[i])
+    end
+    @eval getpara{$(params...), n}(t::Type{Tuple{$(params...)}}, ::Type{Val{n}}) = throw(BoundsError(t, n))
+
+    # Accessing by a non-constant selects the proper value with an ifelse chain
+    if p == 1
+        ie = :(P1)
+    elseif p > 1
+        ie = :(ifelse(i == $p, $(params[p]), $ie))
+    end
+    @eval getpara{$(params...)}(t::Type{Tuple{$(params...)}}, i::Int) = (1 <= i <= $p || throw(BoundsError(t, i)); $ie)
+    # It'd be nice to simply define the constant `i` computation in the type 
+    # domain instead of relying upon dispatch, but it's not constant-folding:
+    # @eval getparam{$(params...), i}(t::Type{Tuple{$(params...)}}, ::Type{Val{i}}) = (1 <= i <= $p || throw(BoundsError(t, i)); $ie)
+
+    # concatenation
+    for q=0:N
+        qarams = ntuple(i->symbol(:Q,i), q)
+        @eval concatenate{$(params...), $(qarams...)}(::Type{Tuple{$(params...)}},::Type{Tuple{$(qarams...)}}) = Tuple{$(params...), $(qarams...)}
+    end
+end
+
+
 # Might as well use this for all datatypes
 function getpara(T::Type)
     check(T)
